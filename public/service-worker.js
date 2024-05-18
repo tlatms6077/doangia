8a964d2680b1dc074f262651b1244b9c2fb186fd
tlatms6077 <tlatms6076@gmail.com>
2024-05-18 10:40:36 +0700

    Update service worker to handle cache errors

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,5 +1,3 @@
-// public/service-worker.js
-
 const CACHE_NAME = 'my-cache-v1';
 const urlsToCache = [
   '/',
@@ -13,12 +11,13 @@ const urlsToCache = [
 ];
 
 self.addEventListener('install', (event) => {
-  // Perform install steps
   event.waitUntil(
     caches.open(CACHE_NAME)
       .then((cache) => {
-        console.log('Opened cache');
-        return cache.addAll(urlsToCache);
+        return cache.addAll(urlsToCache)
+          .catch((error) => {
+            console.error('Failed to cache', error);
+          });
       })
   );
 });
@@ -42,28 +41,19 @@ self.addEventListener('fetch', (event) => {
   event.respondWith(
     caches.match(event.request)
       .then((response) => {
-        // Cache hit - return response
         if (response) {
           return response;
         }
         return fetch(event.request).then(
           (response) => {
-            // Check if we received a valid response
             if (!response || response.status !== 200 || response.type !== 'basic') {
               return response;
             }
-
-            // IMPORTANT: Clone the response. A response is a stream
-            // and because we want the browser to consume the response
-            // as well as the cache consuming the response, we need
-            // to clone it so we have two streams.
             const responseToCache = response.clone();
-
             caches.open(CACHE_NAME)
               .then((cache) => {
                 cache.put(event.request, responseToCache);
               });
-
             return response;
           }
         );
