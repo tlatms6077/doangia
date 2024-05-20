8e40cbfad124c9f8ee03d45ba3d4df2b46464c56
tlatms6077 <tlatms6076@gmail.com>
2024-05-20 08:29:25 +0700

    index.tsx updated

--- src/index.tsx
+++ src/index.tsx
@@ -1,15 +1,24 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import ReactDOM from 'react-dom/client'; // 수정된 임포트 경로
 import './index.css';
 import App from './App';
-import * as serviceWorkerRegistration from './serviceWorkerRegistration'; // 서비스 워커 모듈을 불러옵니다.
+import * as serviceWorkerRegistration from './serviceWorkerRegistration';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+const rootElement = document.getElementById('root');
 
-// 서비스 워커를 등록합니다.
-serviceWorkerRegistration.register();
+if (rootElement) {
+  // 'root' DOM 노드에 대한 루트를 생성합니다.
+  const root = ReactDOM.createRoot(rootElement);
+
+  // 루트 렌더링을 수행합니다.
+  root.render(
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  );
+
+  // 서비스 워커를 등록합니다.
+  serviceWorkerRegistration.register();
+} else {
+  console.error("Failed to find the root element. Make sure the element with id 'root' exists in your HTML.");
+}
