972a2604438012a216633a1bbd7c9f2579714f04
tlatms6077 <tlatms6076@gmail.com>
2024-05-23 08:26:40 +0700

    update

--- server.js
+++ server.js
@@ -6,29 +6,26 @@ require('dotenv').config();
 const app = express();
 const port = 4000;
 
-// CORS 설정: 특정 출처 허용
+const allowedOrigins = ['http://localhost:3000', 'https://tlatms6077.github.io'];
+
 app.use(cors({
-    origin: 'https://tlatms6077.github.io',
-    methods: ['GET', 'POST', 'OPTIONS'],
-    allowedHeaders: ['Content-Type', 'Authorization'],
-    optionsSuccessStatus: 200
+  origin: function (origin, callback) {
+    if (!origin) return callback(null, true);
+    if (allowedOrigins.indexOf(origin) === -1) {
+      const msg = 'The CORS policy for this site does not allow access from the specified Origin.';
+      return callback(new Error(msg), false);
+    }
+    return callback(null, true);
+  }
 }));
 
-// JSON 바디 파싱을 위해 미들웨어 추가
-app.use(express.json());
-
-// OPTIONS 요청에 대한 처리 추가
-app.options('/api/notion-data', cors());
+app.use(express.json()); // JSON 바디 파싱을 위해 추가
 
 const NOTION_API_URL = `https://api.notion.com/v1/databases/${process.env.NOTION_DATABASE_ID}/query`;
 const NOTION_API_KEY = process.env.NOTION_KEY;
 
 app.post('/api/notion-data', async (req, res) => {
-    const { email } = req.body; // 클라이언트에서 전송된 이메일 주소
-    if (!email) {
-        return res.status(400).json({ error: 'Email is required' });
-    }
-
+    const userEmail = req.body.email; // 클라이언트에서 전송된 이메일 주소
     try {
         const response = await axios.post(NOTION_API_URL, {}, {
             headers: {
@@ -40,11 +37,11 @@ app.post('/api/notion-data', async (req, res) => {
 
         const filteredResults = response.data.results.filter(item => {
             const people = item.properties['사람']?.people || [];
-            return people.some(person => person.person.email === email);
+            return people.some(person => person.person.email === userEmail);
         });
 
         if (filteredResults.length === 0) {
-            return res.status(404).json({ error: 'Invalid email. Please try again.' });
+            return res.status(404).json({ error: '잘못된 이메일입니다. 다시 입력해주세요.' });
         }
 
         console.log('Filtered Data:', filteredResults);  // 필터링된 데이터를 로그에 출력
